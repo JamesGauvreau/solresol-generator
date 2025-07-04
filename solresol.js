723d725ca27721c1b643678b844b94a5438ca849
cmSalticidae <james.longfrenchname@gmail.com>
2025-07-04 00:50:45 -0700

    outline of solresol transcriber

---
+++ solresol.js
@@ -0,0 +1,29 @@
+// TODO
+//      Rename "solresol-transcriber" | it transcribes, does not translate
+// !    HTML   
+//      Buttons to press in order to input code
+//      Text field to type or paste
+//      * JS must be able to confirm that input is parseable
+//      * If input is wrong, must return error
+//      * When returning error, must highlight error
+//      Buttons to determine output
+//      ?   Symbol (double note = crossed symbol)
+//          * Some way to combine symbols in a pleasing manner?
+//          * Generally words not longer than five syllables, so brute force is possible
+//      ?   Color
+//          * Customize color.
+//      ?   Music note (visual)
+//      ?   Music note (audio)
+//      ?   Hand sign
+//      ?   Shavian
+//      ?   Ascii
+//      ?   Braille
+//      ?   Binary
+//      ?   George Boeree symbols
+//      * Some output can potentially be mixed
+// !    JS
+//      Take input from text field
+//      Determine preferred output style
+//      return appropriate output
+
+//      https://wiki.xxiivv.com/site/solresol.html
